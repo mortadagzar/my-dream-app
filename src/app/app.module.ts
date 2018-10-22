99b2dd5624e7816794752e3c1939d8195c7295a5
mortada gzar <mortadagzar@gmail.com>
2018-10-21 21:34:19 -0700

    Update src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServerlolComponent } from './serverlol/serverlol.component';
+import { thirdappComponent } from './thirdapp/thirdapp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServerlolComponent
+    ServerlolComponent,
+    thirdapp
   ],
   imports: [
     BrowserModule,
